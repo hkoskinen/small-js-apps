27bb68d2761513475da9bb591125b400c50c83ea
Henri Koskinen <hkoskinen@protonmail.com>
2019-02-18 13:16:39 +0200

    Add the ability to add books, delete them and add some example data

--- book-collector/assets/js/app.js
+++ book-collector/assets/js/app.js
@@ -1,15 +1,53 @@
 (() => {
   'use strict';
 
-  document.addEventListener('DOMContentLoaded', () => {
-    // get the tbody element
-    const bookList = document.querySelector('.book-list'); 
+  // UI variables
+  const bookForm = document.querySelector('#book-form'),
+        title = document.querySelector("#title"),
+        author = document.querySelector("#author"),
+        isbn = document.querySelector("#isbn");
 
-    // read the book data from somewhere else, perhaps from localStorage?
+  const bookList = document.querySelector('.book-list');
+  const errorMessage = document.querySelector('#error-message');
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    // add some example data
     bookList.appendChild(createBookItem('The Art Of Deception', 'Kevin Mitnick', '978-0764542800'));
-    bookList.appendChild(createBookItem('The Nature of Code', 'Daniel Shiffman', '978-0985930806'));
+    bookList.appendChild(createBookItem('The Nature Of Code: Simulating Natural Systems with Processing', 'Daniel Shiffman', '978-0985930806'));
+    bookList.appendChild(createBookItem('Code: The Hidden Language of Computer Hardware and Software', 'Charles Petzold', '978-0735611313'));
+    bookList.appendChild(createBookItem("Clean Architecture: A Craftsman's Guide to Software Structure and Design", 'Robert C. Martin', '978-0134494166'));
+    bookList.appendChild(createBookItem('Effective Java', 'Joshua Bloch', '978-0134685991'));
+  });
+
+  bookForm.addEventListener('submit', e => {
+    if (title.value === '' || author.value === '' || isbn.value === '') {
+      showError('Please fill out all the fields');
+    } else {
+      bookList.appendChild(createBookItem(title.value, author.value, isbn.value));
+      clearForm();
+    }
+    e.preventDefault();
+  });
+
+  bookList.addEventListener('click', e => {
+    // make sure that you can press the whole button to delete the row
+    if (e.target.classList.contains('delete-book')) {
+      e.target.parentElement.parentElement.remove();
+    }
   });
   
+  function showError(message) {
+    errorMessage.textContent = message;
+    errorMessage.style.display = 'block';
+    setTimeout(() => {
+      errorMessage.style.display = 'none';
+    }, 3000);
+  }
+
+  function clearForm() {
+    bookForm.reset();
+  }
+
   function createBookItem(title, author, isbn) {
     const tr = document.createElement('tr');
     const titleEl = document.createElement('td');
@@ -26,14 +64,13 @@
 
     const deleteEl = document.createElement('td');
     deleteEl.classList = 'center';
-    const anchor = document.createElement('a');
-    anchor.href = '#';
-    anchor.classList = "delete-book";
-    anchor.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    deleteEl.appendChild(anchor);
+    const btn = document.createElement('button');
+    btn.classList = "delete-book pure-button";
+    btn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteEl.appendChild(btn);
     tr.appendChild(deleteEl);
 
     return tr;
   }
-
+  
 })();
